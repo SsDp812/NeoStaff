64a86381ac9c1d41eee06c85cc272d22141b1ee4
teamtobi1 <81513375+teamtobi1@users.noreply.github.com>
2023-06-09 15:02:39 +0400

    Union ActionRepositoryService and ActionService

--- business/src/main/java/org/neoflex/business/ActionService.java
+++ business/src/main/java/org/neoflex/business/ActionService.java
@@ -1,5 +1,6 @@
 package org.neoflex.business;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import org.neoflex.business.mapper.ActionMapper;
@@ -13,21 +14,33 @@ import org.neoflex.model.Action;
 import org.neoflex.model.User;
 import org.neoflex.repository.ActionRepository;
 import org.neoflex.repository.UserRepository;
+import org.neoflex.repository.specification.ActionSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.Period;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ActionService {
-    private ActionRepository actionRepository;
-    private UserRepository userRepository;
+    private final ActionRepository actionRepository;
+    private final UserRepository userRepository;
+
+    private final NotificationService notificationService;
+
+    @Value("${action.minutesBeforeNotification}")
+    private long minutesBeforeNotification;
 
     @Autowired
-    public ActionService(ActionRepository actionRepository) {
+    public ActionService(ActionRepository actionRepository, UserRepository userRepository, NotificationService notificationService) {
         this.actionRepository = actionRepository;
+        this.userRepository = userRepository;
+        this.notificationService = notificationService;
     }
 
     public ActionCardDto addActionToUser(@Valid AddActionToUserDto dto) throws UserNotFoundException {
@@ -109,5 +122,36 @@ public class ActionService {
         } else {
             throw new ActionNotFound();
         }
+    }
+
+    @Scheduled(cron = "${action.repeat.cron}")
+    @Transactional
+    public void repeatActions() {
+        List<Action> repeatableActions = actionRepository.findAll(ActionSpecification.getAllRepeatables());
+        var newActions = repeatableActions.stream()
+                .map(this::getRepeatAction)
+                .toList();
+        actionRepository.saveAll(newActions);
+    }
+
+    @Scheduled(cron = "${action.notification.cron}")
+    @Transactional
+    public void notifyUser() {
+        ZonedDateTime actionDate = ZonedDateTime.now().plusMinutes(minutesBeforeNotification);
+        List<Action> actions = actionRepository.findAll(ActionSpecification.getAllNeedNotify(actionDate));
+
+        actions.forEach(notificationService::sendNotifications);
+    }
+
+    @Transactional
+    public Action getRepeatAction(Action action) {
+        Period interval = action.getType().getInterval();
+        return new Action(
+                null,
+                action.getUserInfo(),
+                action.getType(),
+                ZonedDateTime.from(interval.addTo(action.getDate())),
+                action.getComment(),
+                false);
     }
 }
